9f6fbd6cd2118ac831405e4a902022329103f1ce
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-27 19:20:38 +0200

    route post(/reports) completed
    small changes in data and report-schema

--- api/controllers.js
+++ api/controllers.js
@@ -9,12 +9,13 @@ const util = require("util");
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 
-const SCHEMA = path.join(
+const SCHEMA_PATH = path.join(
   __dirname,
   "/..",
   config.DATA_DIR,
   "/report-schema.json"
 );
+const File_SCHEMA = require(SCHEMA_PATH);
 const DATA_PATH = path.join(
   __dirname,
   "/..",
@@ -45,6 +46,51 @@ const controllers = {
       }
       res.json(findReport);
     } catch (error) {
+      res.status(500).send("Something went wrong, please try later.");
+    }
+  },
+  addReport: async (req, res) => {
+    const newReport = {
+      id: 0,
+      have_electricity: req.body.have_electricity,
+      city: req.body.city,
+      state: req.body.state,
+      date: "",
+    };
+
+    try {
+      const content = await readFile(DATA_PATH, "utf-8");
+      const data = JSON.parse(content);
+
+      newReport.id = data.nextId;
+      data.nextId++;
+
+      const date_time = new Date();
+      const formattedDate = `${date_time.toDateString()} ${date_time.toLocaleTimeString()}`;
+      newReport.date = formattedDate;
+
+      const isValid = tv4.validate(newReport, File_SCHEMA);
+
+      if (!isValid) {
+        const error = tv4.error;
+        console.error(error);
+
+        res.status(400).json({
+          error: {
+            message: error.message,
+          },
+        });
+        return;
+      }
+
+      data.reports.push(newReport);
+      const newFileDataString = JSON.stringify(data, null, "  ");
+
+      await writeFile(DATA_PATH, newFileDataString);
+
+      res.json(newReport);
+    } catch (error) {
+      console.log(error);
       res.status(500).send("Something went wrong, please try later.");
     }
   },
