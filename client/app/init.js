1d989e76804d27f941a097922b58f31822a39903
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-28 18:32:17 +0200

    I can get the reports in init.js

--- client/app/init.js
+++ client/app/init.js
@@ -1,9 +1,15 @@
-fetch('/api')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    document.getElementById('root')
-      .innerHTML = data.message;
-  })
-  .catch(err => console.error(err));
+const init = async () => {
+  const res = await fetch("api/reports");
+  const data = await res.json();
+  console.log(data);
+};
 
+init();
+
+// fetch("/api/reports")
+//   .then((res) => res.json())
+//   .then((data) => {
+//     console.log(data);
+//     document.getElementById("reports-container").innerHTML = "hola";
+//   })
+//   .catch((err) => console.error(err));
