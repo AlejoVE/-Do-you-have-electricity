28aaf28cbb041ddad8c1f3babb82ce97f18cc481
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-05-29 00:26:44 +0200

    I added view.js with renderReports method

--- client/app/init.js
+++ client/app/init.js
@@ -1,7 +1,13 @@
 const init = async () => {
   const res = await fetch("api/reports");
   const data = await res.json();
-  console.log(data);
+
+  const mainDivTrue = document.getElementById("reports-container-true");
+  const mainDivFalse = document.getElementById("reports-container-false");
+  const divElTrue = views.renderReports(data, true);
+  const divElFalse = views.renderReports(data, false);
+  mainDivTrue.appendChild(divElTrue);
+  mainDivFalse.appendChild(divElFalse);
 };
 
 init();
---
+++ client/app/views.js
@@ -0,0 +1,88 @@
+const views = {
+  renderReports: (arrayReports, value) => {
+    const reports = arrayReports;
+    const theyHaveElectricity = [];
+    const theyDoNotHaveElectricity = [];
+
+    reports.forEach((report) => {
+      if (report.have_electricity === true) {
+        theyHaveElectricity.push(report);
+      } else {
+        theyDoNotHaveElectricity.push(report);
+      }
+    });
+
+    const divElTrue = document.createElement("div");
+    const divElFalse = document.createElement("div");
+
+    theyDoNotHaveElectricity.forEach((report) => {
+      const divEl = createDiv(report);
+      divElFalse.appendChild(divEl);
+    });
+
+    theyHaveElectricity.forEach((report) => {
+      const divEl = createDiv(report);
+      divElTrue.appendChild(divEl);
+    });
+
+    if (value === true) {
+      return divElTrue;
+    }
+
+    return divElFalse;
+  },
+};
+
+function createDiv(report) {
+  const divEl = document.createElement("div");
+  const ulEl = document.createElement("ul");
+  const li = document.createElement("li");
+  let haveElectricity = "yes";
+
+  if (report.have_electricity === false) {
+    haveElectricity = "no";
+  }
+
+  li.innerHTML = `<details close>
+  <summary>Report N°: ${report.id}. Place: ${report.city} </summary>
+    <form>
+      <fieldset disabled>
+        <label>State:</label>
+        <input type="text" value="${report.state}" />
+        <br />
+        <label>City:</label>
+        <input type="text" value="${report.city}" />
+        <br />
+        <label>Street address:</label>
+        <input type="text" value="${report.street_address}" />
+        <br />
+        <label>Have Electricity:</label>
+        <input type="text" value="${haveElectricity}" />
+        <br />
+        <p><strong>Date:</strong> ${report.date}</p>
+      </fieldset>
+    </form>
+</details>
+`;
+  ulEl.appendChild(li);
+  divEl.appendChild(ulEl);
+  return divEl;
+}
+
+// `<form>
+//     <fieldset disabled>
+//         <label>State:</label>
+//         <input type=text value="${report.state}">
+//        <br>
+//        <label>City:</label>
+//         <input type=text value="${report.city}">
+//         <br>
+//         <label>Street address:</label>
+//         <input type=text value="${report.street_address}">
+//         <br>
+//         <label>Have Electricity:</label>
+//         <input type="text" value="yes">
+//         <br>
+//         <p>Date: ${report.date}</p>
+//     </fieldset>
+// </form>`
